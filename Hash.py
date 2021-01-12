2fed7e08e2ab57cf962d1fb75e523eb84bc95d12
FrankHare <frank.hare.16@gmail.com>
2021-01-11 20:13:48 -0400

    change hash_code calculation
    change raw data to 8-bit string, then hash_code it to a big number by times 10**i to scatter them into bucket
    double hash works, but freezes occasionally
    change bucket size to the larger one of twin primes

--- Hash.py
+++ Hash.py
@@ -11,15 +11,15 @@ class HashTable: #Hash Table Specification
     def __init__(self, M, hf, step):
         self.size = M # Prime number of bucket size
         self.hf = hf # Hash function: modular or multiplicative
-        self.step =  step # quadratic probing if step == 0, else linear probing
+        self.step =  step # <-1: double hash / -1: chaining / 0: quadratic probing / >0: linear probing
         
-        self.ch = "ch" if step < 0 else "lp" # Collision handling strategy
+        self.ch = "ch" if step == 0 else "lp" # Collision handling strategy
         self.mf = (sqrt(5)-1)/2 # (3^40)/(2^64) Use golden ratio as mulplicative factor
         
         #[] for chaining, None for linear probing
-        if self.ch == "lp": # linear probing
+        if self.ch == "lp": # open addressing / closed hashing
             self.bucket = [None for generation in range(self.size)] 
-        elif self.ch == "ch": # chaining
+        elif self.ch == "ch": # seperate chaining / open hashing
             self.bucket = [[] for generation in range(self.size)]
 
     #print every key and its index/chain
@@ -37,36 +37,70 @@ class HashTable: #Hash Table Specification
     def memory_usage(self):
         return getsizeof(self.bucket)
 
+    # hash code: convert string to integer
+    def hash_code(self, key):
+        hash_code = 0
+        for i in range(len(key)):
+            hash_code += ord(key[i]) * (10**i)
+        return hash_code
+
     # hash function, return index
     def hash_function(self, key):
-        hash_code = 0
-        for character in key:
-            hash_code += ord(character)
+        hash_code = self.hash_code(key)
+
         if self.hf == "mod":
             return hash_code % self.size
         elif self.hf == "mul":
             return floor(self.size * modf(hash_code * self.mf)[0])
 
+    # double hash function: index = index0 + j * hf2(key,j)
+    def double_hash_function(self, key, j):
+        if self.step > 0: # linear probing
+            return self.step
+        elif self.step == -1: # quadratic probing
+            return j
+        elif self.step == -2 or self.step == -3: # double hash modular function
+            hash_code = self.hash_code(key)
+            return hash_code % (self.size-2) # better if self.size and self.size-2 are twin primes
+        elif self.step == -4 or self.step == -5: # double hash mulplicative function
+            hash_code = self.hash_code(key)
+            return floor((self.size-2) * modf(hash_code * self.mf)[0])
+
     # insert key, return bucket index
     def __setitem__(self, key): 
-        index = self.hash_function(key)
+        index = self.hash_function(key) # index0
+        #step0 = self.double_hash_function(key,0)
+
         if self.ch == "ch":
             self.bucket[index].append(key)
         elif self.ch == "lp":
             j = 0
             while self.bucket[index] is not None:
                 j += 1
-                # quadratic probing if step = 0, step = (index0 + j^2) - (index0 + (j-1)^2) = j^2 - (j-1)^2 = 2*j-1
-                # linear probing if step > 0
-                index = index+2*j-1 if self.step == 0 else index+self.step
+                # index = index0 + j * hf2(key,j)
+                # linear probing    if step  >  0: hf2(key,j) = step            => step_j = (index0 + j*step) - (index0 + (j-1)*step) = step
+                # quadratic probing if step  = -1: hf2(key,j) = j               => step_j = (index0 + j^2) - (index0 + (j-1)^2) = j^2 - (j-1)^2 = 2*j-1
+                # double hash       if step  < -1: hf2(key,j) = step0 + (j+1)/2 => step_j = (index0 + j*(step0+(j+1)/2)) - (index0 + (j-1)*(step0+(j-1+1)/2)) = step0 + j
+                if self.step > 0 : # linear probing
+                    index = index + self.step
+                else: #elif self.step == -1 : # quadratic probing
+                    index = index + 2*j-1
+                #elif self.step == -2 or self.step == -4: # double hash without j in hf2(key,j) = step0: it is just key-varied linear probing
+                #    index = index + step0
+                #elif self.step == -3 or self.step == -5: # double hash with j in hf2(key,j) = step0 + (j+1)/2
+                #    index = index + step0 + j
+                
                 index = index % self.size
+            
             self.bucket[index] = key
             
         return index
 
     # query key, return chain location
     def __getitem__(self, key): 
-        index = self.hash_function(key)
+        index = self.hash_function(key) # index0
+        #step0 = self.double_hash_function(key,0)
+
         if self.ch == "ch":
             for j in range(len(self.bucket[index])):
                 if self.bucket[index][j] == key:
@@ -75,10 +109,18 @@ class HashTable: #Hash Table Specification
             j = 0
             while self.bucket[index] != key:
                 j += 1
-                # quadratic probing if step = 0, step = (index0 + j^2) - (index0 + (j-1)^2) = j^2 - (j-1)^2 = 2*j-1
-                # linear probing if step > 0
-                index = index+2*j-1 if self.step == 0 else index+self.step
+
+                if self.step > 0 : # linear probing
+                    index = index + self.step
+                else: #elif self.step == -1 : # quadratic probing
+                    index = index + 2*j-1
+                #elif self.step == -2 or self.step == -4: # double hash without j in hf2(key,j) = step0: it is just key-varied linear probing
+                #    index = index + step0
+                #elif self.step == -3 or self.step == -5: # double hash with j in hf2(key,j) = step0 + (j+1)/2
+                #    index = index + step0 + j
+
                 index = index % self.size
+
             return j
             
         raise Exception("Key does not exist in Hash Table")
@@ -104,8 +146,9 @@ def main():
     letters = string.ascii_letters + string.digits
 
     # test different M for modular and mulplicative funcation
-    operations = ([9999991,"mod"], [19997,  "mod"], [11113,  "mod"], [10193,  "mod"], [10037,  "mod"], [10007,  "mod"],
-                  [9999991,"mul"], [19997,  "mul"], [11113,  "mul"], [10193,  "mul"], [10037,  "mul"], [10007,  "mul"])
+    operations = ([9999973, "mod"], [19993, "mod"], [11119, "mod"], [10429, "mod"], [10039, "mod"], [10009, "mod"],
+                  [9999973, "mul"], [19993, "mul"], [11119, "mul"], [10429, "mul"], [10039, "mul"], [10009, "mul"])
+    #             [9999991, "mul"], [19997, "mul"], [11113, "mul"], [10193, "mul"], [10037, "mul"], [10007, "mul"])
 
     # repeat round for a group of operations and steps
     for r in range(roundN): 
@@ -115,7 +158,7 @@ def main():
         # create a new raw dataset for each new round
         keys = []
         for i in range(N):
-            keys.append("".join([random.choice(letters) for i in range(random.randint(1,16))]))  # random 16-bit password as key
+            keys.append("".join([random.choice(letters) for i in range(random.randint(1,8))]))  # random 8-bit password as key
 
         # create a new output csv file for each new round
         file = open(f"/Project/Hash/Hash{str(r).zfill(3)}.csv","a")
@@ -132,9 +175,10 @@ def main():
                      + ",Mu5InTm,Mu5RtTm,Mu5CMed,Mu5CAvg,Mu5CStd"
                      + ",Mu6InTm,Mu6RtTm,Mu6CMed,Mu6CAvg,Mu6CStd")
 
-        # step < 0: chaining; 
-        # step = 0: quadratic probing; 
-        # step > 0: linear probing
+        # step < -1: open addressing / closed hashing: double hash
+        # step = -1: open addressing / closed hashing: quadratic probing
+        # step =  0: seperate chaining / open hashing
+        # step >  0: open addressing / closed hashing: linear probing
         for step in range(-1,stepN):
             # start with a return and step for each new row
             file.write(f"\n{step}")
